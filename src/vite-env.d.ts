3f165450afc0fb76f78c603dfd5268e02ab10274
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-26 08:15:37 +0900

    configure vite-plugin-svgr

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module "*.svg" {
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+
+  // eslint-disable-next-line import/no-default-export
+  export default src;
+}
