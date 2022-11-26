a9e0fff8291c3ad8492b78ee6a22b21bbf093d42
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-26 09:48:07 +0900

    update router for BlogPage

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,20 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import { BlogPage } from './pages/BlogPage'
 
-export enum routes {}
+export enum routes {
+  BLOG = "/blog/:blogName"
+}
 
-const router = createBrowserRouter([])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to={routes.BLOG} />
+  },
+  {
+    path: routes.BLOG,
+    element: <BlogPage />
+  },
+])
 
 export const AppRouter: React.FC = () => {
   return <RouterProvider router={router} />
