a5018445a1674a7d0913a47ff85b463b3d08d845
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-28 00:49:01 +0900

    create style utils

---
+++ src/utils/style/text.css.ts
@@ -0,0 +1,43 @@
+import { style } from '@vanilla-extract/css'
+
+export const fontSize = {
+  px8: style({
+    fontSize: '8px',
+  }),
+  px10: style({
+    fontSize: '10px',
+  }),
+  px12: style({
+    fontSize: '12px',
+  }),
+  px16: style({
+    fontSize: '16px',
+  }),
+  px20: style({
+    fontSize: '20px',
+  }),
+  px24: style({
+    fontSize: '24px',
+  }),
+}
+
+export const lineHeight = {
+  per150: style({
+    lineHeight: '1.5',
+  }),
+  per175: style({
+    lineHeight: '1.75',
+  }),
+  per200: style({
+    lineHeight: '2',
+  }),
+}
+
+export const fontWeight = {
+  normal: style({
+    fontWeight: 'normal',
+  }),
+  bold: style({
+    fontWeight: 'normal',
+  }),
+}
---
+++ src/utils/style/variables.ts
@@ -0,0 +1,7 @@
+export const color = {
+  black: '#1F1F1F',
+  white: '#E8E8E8',
+  primary: '#FFC107',
+  primaryLight: '#FFDA6B',
+  gray: '#B6B6B6',
+}
