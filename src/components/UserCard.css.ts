18ee1f6235b58ce60d644126329d2dc3fe2ad43e
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-28 01:45:57 +0900

    update UserCard style

--- src/components/UserCard.css.ts
+++ src/components/UserCard.css.ts
@@ -1,10 +1,10 @@
 import { fontSize, fontWeight, lineHeight } from '@/utils/style/text.css'
 import { style } from '@vanilla-extract/css'
 
-export const name = style([fontSize.px16, fontWeight.bold, lineHeight.per150])
+export const name = style([fontSize.px20, fontWeight.bold, lineHeight.per150])
 
 export const introduction = style([
-  fontSize.px12,
+  fontSize.px14,
   fontWeight.normal,
   lineHeight.per175,
 ])
