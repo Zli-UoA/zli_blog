b911d60e4d8423fae52fe111586a939912331ba8
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-28 01:32:41 +0900

    fix mdMetaData

--- src/utils/lib/mdMetaData.ts
+++ src/utils/lib/mdMetaData.ts
@@ -1,24 +1,27 @@
-export const extractMetaData = <T extends Record<string, string[]>>(text: string): Partial<T> => {
-  const metaRegExp = RegExp(/^---[\r\n](((?!---).|[\r\n])*)[\r\n]---$/m);
+export const extractMetaData = <T extends Record<string, string[]>>(
+  text: string
+): Partial<T> => {
+  const metaRegExp = RegExp(/^---[\r\n](((?!---).|[\r\n])*)[\r\n]---$/m)
 
-  const rawMetaData = metaRegExp.exec(text);
+  const rawMetaData = metaRegExp.exec(text)
 
   if (!rawMetaData) {
     return {}
   }
 
-  return rawMetaData[1].split("\n")
+  return rawMetaData[1]
+    .split('\n')
     .map((keyValue) => {
-      const [key, value] = keyValue.split(":");
+      const [key, value] = keyValue.split(':')
 
       return {
-        [key]: value.trim().split(" ")
+        [key]: value.trim().split(' '),
       }
     })
-    .reduce((a, b) => ({ ...a, ...b })) as Partial<T>;
+    .reduce((a, b) => ({ ...a, ...b })) as Partial<T>
 }
 
 export const removeMetaData = (text: string): string => {
-  const metaRegExp = RegExp(/^---[\r\n](((?!---).|[\r\n])*)[\r\n]---$/m);
-  return text.replace(metaRegExp, "");
+  const metaRegExp = RegExp(/^---[\r\n](((?!---).|[\r\n])*)[\r\n]---$/m)
+  return text.replace(metaRegExp, '')
 }
