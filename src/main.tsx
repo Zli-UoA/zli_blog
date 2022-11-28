29714ef580478c73511b3e44fa45402e68802b7d
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-28 09:23:22 +0900

    fix path

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import 'the-new-css-reset/css/reset.css'
-import { AppRouter } from './router'
+import { AppRouter } from '@/Router'
 import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
