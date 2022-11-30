3ce9a586f2f7af550478205a8ffb00a66a8d21af
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-30 15:29:05 +0900

    create BlogListPage

---
+++ src/pages/BlogListPage/BlogListPageMobile.tsx
@@ -0,0 +1,59 @@
+import { MobileDrawerMenu } from '@/components/MobileDrawerMenu'
+import { BlogInfo } from '@/models/BlogInfo'
+import { Header } from '@/utils/components/Header'
+import { Page } from '@/utils/components/Page'
+import { PageTitle } from '@/utils/components/PageTitle'
+import { Spacer } from '@/utils/components/Spacer'
+import { HStack, VStack } from '@/utils/components/Stack'
+import { useOpen } from '@/utils/hooks/useOpen'
+import { Link } from 'react-router-dom'
+import { ReactComponent as Logo } from '@/assets/Logo.svg'
+import { ReactComponent as MenuIcon } from '@/assets/Menu.svg'
+import { FooterLink } from '.'
+import { ItemList } from '@/utils/components/ItemList'
+import { BlogCard } from '@/components/BlogCard'
+
+export const BlogListPageMobile: React.FC<{
+  blogs: BlogInfo[] | undefined
+  footerLinks: FooterLink[]
+}> = ({ blogs, footerLinks }) => {
+  const drawer = useOpen(false)
+
+  return (
+    <Page title="Blog">
+      <Header>
+        <HStack
+          style={{
+            alignItems: 'center',
+            gap: 12,
+          }}
+        >
+          <button onClick={drawer.open}>
+            <MenuIcon />
+          </button>
+          <Link to="/">
+            <Logo />
+          </Link>
+        </HStack>
+      </Header>
+      <main>
+        <VStack style={{ alignItems: 'center' }}>
+          <Spacer size={40} />
+          <PageTitle>Blog</PageTitle>
+          <Spacer size={40} />
+          <ItemList>
+            {blogs?.map((blog, i) => {
+              return <BlogCard blog={blog} key={i} />
+            })}
+          </ItemList>
+          <Spacer size={80} />
+        </VStack>
+      </main>
+      <MobileDrawerMenu
+        isOpen={drawer.isOpen}
+        close={drawer.close}
+        footerLinks={footerLinks}
+      />
+    </Page>
+  )
+}
---
+++ src/pages/BlogListPage/index.tsx
@@ -0,0 +1,53 @@
+import { getBlogInfoList } from '@/api/getBlogInfoList'
+import { BlogInfo } from '@/models/BlogInfo'
+import { useMobile } from '@/utils/hooks/useMobile'
+import { useEffect, useState } from 'react'
+import { BlogListPageMobile } from './BlogListPageMobile'
+import { BlogListPagePC } from './BlogListPagePC'
+
+export type FooterLink = {
+  url: string
+  label: string
+}
+
+const footerLinks = [
+  { label: 'Twitter', url: 'https://twitter.com/zliofficial' },
+  { label: 'Connpass', url: 'https://zli.connpass.com/' },
+  { label: 'Qiita', url: 'https://qiita.com/organizations/zli' },
+  { label: 'GitHub', url: 'https://github.com/zli-UoA' },
+]
+
+const useBlogListPage = (): {
+  data?: BlogInfo[]
+  loading: boolean
+} => {
+  const [data, setData] = useState<BlogInfo[]>()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const hoge = async (): Promise<void> => {
+      setLoading(true)
+      setData(await getBlogInfoList())
+      setLoading(false)
+    }
+
+    hoge().catch((e) => console.log(e))
+  }, [])
+
+  return {
+    data,
+    loading,
+  }
+}
+
+export const BlogListPage: React.FC = () => {
+  const isMobile = useMobile()
+
+  const { data } = useBlogListPage()
+
+  if (isMobile) {
+    return <BlogListPageMobile blogs={data} footerLinks={footerLinks} />
+  }
+
+  return <BlogListPagePC blogs={data} footerLinks={footerLinks} />
+}
