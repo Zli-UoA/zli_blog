1d2e1a38ffa945c3355c1acf2ccb28177a86f313
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-12-03 09:56:57 +0900

    getBlogを生のマークダウンではなく、Blog型を返すように修正

--- src/api/getBlog.ts
+++ src/api/getBlog.ts
@@ -1,4 +1,21 @@
-export const getBlog = async (blogName: string): Promise<string> => {
+import { Blog } from "@/models/Blog";
+import { extractMetaData, removeMetaData } from "@/utils/lib/mdMetaData";
+
+export const getBlog = async (blogName: string): Promise<Blog> => {
   const res = await fetch('/articles/' + blogName + '/index.md');
-  return await res.text()
+  const data = await res.text()
+
+  const metaData = extractMetaData<{
+    authorId: [string]
+    title: [string]
+    tags: string[]
+  }>(data)
+
+  return {
+    authorId: metaData.authorId ? metaData.authorId[0] : "",
+    title: metaData.title ? metaData.title[0] : "",
+    tags: metaData.tags ? metaData.tags : [],
+    eyeCatchUrl: "/articles/" + blogName + "/eyeCatch.png",
+    mdText: removeMetaData(data)
+  }
 }
