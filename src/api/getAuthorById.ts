7cf5bd2e0b7a52553fb9726b97461ab6fd38110f
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-12-03 09:58:14 +0900

    create getAuthorById

---
+++ src/api/getAuthorById.ts
@@ -0,0 +1,13 @@
+import { Author } from "@/models/Author"
+import { removeMetaData } from "@/utils/lib/mdMetaData"
+
+export const getAuthorById = async (authorId: string): Promise<Author> => {
+  const res = await fetch("/authors/" + authorId + "/index.md")
+  const mdData = await res.text()
+
+  return {
+    id: authorId,
+    iconUrl: "/authors/" + authorId + "icon.png",
+    introduction: removeMetaData(mdData),
+  }
+}
