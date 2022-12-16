c3ff1422d445520b5cff17d32a36b00edc5ec162
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-12-16 19:23:28 +0900

    fix Page style

--- src/utils/components/Page.css.ts
+++ src/utils/components/Page.css.ts
@@ -2,9 +2,11 @@ import { style } from '@vanilla-extract/css'
 
 export const page = style({
   padding: '0 16px',
+  minHeight: '100vh',
 })
 
 export const pageInner = style({
+  minHeight: '100vh',
   maxWidth: '730px',
   margin: 'auto',
 })
