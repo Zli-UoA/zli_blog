dabfe2134865fe93e734e86149d1f57b0abb8678
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-12-24 17:18:35 +0900

    データの加工を切り出し

--- src/api/getBlog.ts
+++ src/api/getBlog.ts
@@ -1,21 +1,23 @@
 import { Blog } from '@/models/Blog'
-import { extractMetaData, removeMetaData } from '@/utils/lib/mdMetaData'
+import { getMarkdownBody, getMarkdownMetaData } from '@/utils/lib/markdown'
 
 export const getBlog = async (blogName: string): Promise<Blog> => {
   const res = await fetch('/articles/' + blogName + '/index.md')
-  const data = await res.text()
+  return createBlog(blogName, await res.text())
+}
 
-  const metaData = extractMetaData<{
+const createBlog = (blogName: string, md: string): Blog => {
+  const metaData = getMarkdownMetaData<{
     authorId: [string]
     title: [string]
     tags: string[]
-  }>(data)
+  }>(md)
 
   return {
     authorId: metaData.authorId ? metaData.authorId[0] : '',
     title: metaData.title ? metaData.title[0] : '',
     tags: metaData.tags ? metaData.tags : [],
     eyeCatchUrl: '/articles/' + blogName + '/eyeCatch.png',
-    mdText: removeMetaData(data),
+    mdText: getMarkdownBody(md),
   }
 }
