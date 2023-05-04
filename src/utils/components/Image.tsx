3e5ed8486f24ebb17ae97c22d3088606b96c4f97
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-05-04 18:16:26 +0900

    fallback画像を設定

--- src/utils/components/Image.tsx
+++ src/utils/components/Image.tsx
@@ -1,10 +1,25 @@
 import { RecipeVariants } from '@vanilla-extract/recipes'
+import { useRef, useState } from 'react'
 import { image } from './Image.css'
 
 export const Image: React.FC<{
   alt: string
   src: string
+  fallbackSrc?: string
   style: RecipeVariants<typeof image>
-}> = ({ alt, src, style }) => {
-  return <img src={src} alt={alt} className={image(style)} />
+}> = ({ alt, src: _src, style, fallbackSrc = '/fallback.png' }) => {
+  const [src, setSrc] = useState(_src)
+  const imgRef = useRef<HTMLImageElement>(null)
+
+  return (
+    <img
+      src={src}
+      alt={alt}
+      ref={imgRef}
+      className={image(style)}
+      onError={(): void => {
+        setSrc(fallbackSrc)
+      }}
+    />
+  )
 }
