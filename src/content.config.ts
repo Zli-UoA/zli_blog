307a47dea7aa44486cc2a0ddaa7f213587db417d
39sho <g.39sho@gmail.com>
2025-12-27 22:35:55 +0900

    コンテンツを持ってくる

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, reference, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: './src/content/blog', pattern: '**/index.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
+			authors: reference('authors').array(),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
@@ -16,4 +17,17 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+const authors = defineCollection({
+	// Load Markdown and MDX files in the `src/content/authors/` directory.
+	loader: glob({ base: './src/content/authors', pattern: '**/index.json' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			id: z.string(),
+			displayName: z.string(),
+			bio: z.string(),
+			icon: image(),
+		}),
+});
+
+export const collections = { blog, authors };
