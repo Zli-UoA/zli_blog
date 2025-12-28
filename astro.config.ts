80eaef7aa06919e9d2fdba73cf6483d3cca07b2a
39sho <g.39sho@gmail.com>
2025-12-28 12:15:50 +0900

    npx astro add cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
+  adapter: cloudflare(),
 });
